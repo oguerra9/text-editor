6b5b35287b875be616edb7fdd162f158262a13c0
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-10-05 17:43:46 -0500

    service worker and webpack configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      // webpack plugin that generates our html file and injects our bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
 
+      // injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'just another text editor',
+        short_name: 'JATE',
+        description: 'just another text editor - notes with JavaScript syntax highlighting',
+        background_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96,128,192,256,384,512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    
     module: {
+      // css loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // we use babel-loader in order to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
